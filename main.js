8e4600fcad3309cc06c1900c290ce061046fdca1
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-09-12 15:59:24 +0500

    9 dars

---
+++ main.js
@@ -0,0 +1,25 @@
+let box = document.querySelector(".box")
+
+let inp = document.getElementById("input1")
+let inp2 = document.getElementById("input2")
+let btn = document.querySelector(".btn")
+
+let data = [123456]
+let data1 = [201212]
+
+alert(`sizningi ID ${data} 
+pasword ${data1}`)
+
+btn.addEventListener("click",()=>{
+    if (inp.value == data && inp2.value == data1) {
+        alert("Дабро пожаловать")
+            inp2.value = ''
+      inp.value = ''
+    } else {
+        alert("sizninig ID yoki Psaword toxri emas ")
+        inp2.value = ''
+      inp.value = ''
+    }
+})
+
+
